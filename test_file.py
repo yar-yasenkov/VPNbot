64d34ebafa83823f6e3792cb484317ae5842deeb
Yaroslav Yasenkov <yar-yasenkov@yandex.ru>
2024-11-08 18:09:11 +0300

    added request

--- test_file.py
+++ test_file.py
@@ -1,2 +1,10 @@
-print('this file from github')
-print('new info')
+import requests
+
+api_url = 'http://numbersapi.com/43'
+
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(response.status_code)
